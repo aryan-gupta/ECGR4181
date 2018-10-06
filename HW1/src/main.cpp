3524c2fe65325b3cf128b0d0f91e7e62c55aee97
Aryan Gupta <kuantum.freak@gmail.com>
2018-10-06 12:42:53 -0400

    Add file parsing for opfile

--- HW1/src/main.cpp
+++ HW1/src/main.cpp
@@ -2,7 +2,8 @@
 #include <memory>
 #include <cstring>
 #include <iostream>
-#include <ifstream>
+#include <fstream>
+#include <sstream>
 
 #include "main.hpp"
 #include "parse.hpp"
@@ -19,7 +20,11 @@ int main(int argn, char** args) {
 		return -1;
 	}
 
-	load_file(dat.file);
+	auto opfile = load_file(dat.file);
+
+	for (auto& p : opfile) {
+		std::cout << p.first << "  " << p.second << std::endl;
+	}
 
 	std::cout << dat.cache_size << std::endl;
 	std::cout << dat.block_size << std::endl;
@@ -28,13 +33,48 @@ int main(int argn, char** args) {
 	std::cout << (int)dat.associativity << std::endl;
 }
 
+std::vector<std::pair<Ops, uint64_t>> load_file(static_string_t filename) {
+	std::vector<std::pair<Ops, uint64_t>> ret;
 
-std::vector<std::pair<uint8_t, uint64_t>> load_file(static_string_t filename) {
-	std::ifstream file{ filename };
-	for (
-		auto [idx, line] = std::pair{ size_t{}, std::string{} };
-		std::getline(file, line);
-	) {
+	std::ifstream file{ std::string { filename } };
+	for (std::string line{  }; std::getline(file, line); ) {
+		std::stringstream ss{ line };
+		Ops code;
+		uint64_t location;
 
+		ss >> code >> std::hex >> location >> std::dec;
+
+		ret.emplace_back(code, location);
 	}
+
+	return ret;
+}
+
+std::ostream& operator<< (std::ostream& out, Ops& op) {
+	switch (op) {
+		case Ops::READ: out << "READ"; break;
+		case Ops::WRITE: out << "WRITE"; break;
+		case Ops::FETCH: out << "FETCH"; break;
+	}
+	return out;
+}
+
+std::istream& operator>> (std::istream& in,  Ops& op) {
+	std::string word;
+	in >> word;
+
+	char first = word[0];
+	switch (first) {
+		case 'R': op = Ops::READ; break;
+		case 'W': op = Ops::WRITE; break;
+		case 'F': op = Ops::FETCH; break;
+		default: {
+			int tmp = std::strtol(word.c_str(), nullptr, 10);
+			if (errno == ERANGE) {
+				in.setstate(std::ios::failbit);
+			}
+			op = static_cast<Ops>(tmp);
+		}
+	}
+	return in;
 }
