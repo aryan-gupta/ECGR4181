a47ba1f60fc435ab4332eb26ca0aec25a50434e6
Aryan Gupta <kuantum.freak@gmail.com>
2018-10-06 12:42:53 -0400

    Add file parsing for opfile

--- HW1/src/main.hpp
+++ HW1/src/main.hpp
@@ -5,11 +5,21 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <iostream>
 
 #include "parse.hpp"
 
 extern bool gUseSTDIN;
 
+enum class Ops {
+	READ,
+	WRITE,
+	FETCH
+};
+
 int main(int argn, char** args);
 
-std::vector<std::pair<uint8_t, uint64_t>> load_file(static_string_t filename);
+std::vector<std::pair<Ops, uint64_t>> load_file(static_string_t filename);
+
+std::ostream& operator<< (std::ostream& out, Ops& op);
+std::istream& operator>> (std::istream& in,  Ops& op);
