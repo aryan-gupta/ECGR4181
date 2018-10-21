905c507a079b48f014aec7a54f225acd80a304c1
Aryan Gupta <kuantum.freak@gmail.com>
2018-10-21 14:21:49 -0400

    Reorganize code

--- HW1/src/printer.hpp
+++ HW1/src/printer.hpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+#include "parse.hpp"
+#include "Simulator.hpp"
+
 struct printer {
 	static inline void dot() {
 		std::cout << ".";
@@ -12,15 +15,29 @@ struct printer {
 		std::cout << "\b \b";
 	}
 
-	static inline void intro() {
+	static inline void intro(const ParseData& pd) {
 		/// from ping(8):
 		// For every ECHO_REQUEST sent a period “.” is printed, while for ever ECHO_REPLY received a backspace is
 		// printed. This provides a rapid display of how many packets are being dropped.
 		// Also just realized they misspelled every
 		// I fell like there will be a point where the period is spammed so I may represent = by 10 periods? idk
-		std::cout << "File and parameters successfully loaded." "\n"
-					"For every memory access a period \".\" is printed, while for every memory hit a backspace is "
-					"printed, this provides a rapid display of how many cache misses there are" "\n";
+		std::cout << "File and parameters successfully loaded." << std::endl;
+
+		std::cout << "Cache size...... " << pd.cache_size << std::endl;
+		std::cout << "Block size...... " << pd.block_size << std::endl;
+		std::cout << "Use stdin....... " << pd.use_stdin << std::endl;
+		std::cout << "Replace Policy.. " << pd.replace_policy << std::endl;
+		std::cout << "Associativity... " << (int)pd.associativity << std::endl;
+
+		std::cout << "For every memory access a period \".\" is printed, while for every memory hit a backspace is "
+					 "printed, this provides a rapid display of how many cache misses there are" << std::endl;
+	}
+
+	static inline void result(const Simulator& sim) {
+		std::cout << std::endl << std::endl; // flush buffer and ouput new line
+		std::cout << "================ Results ================\n";
+		auto ratio = sim.getRatio();
+		std::cout << "Hits: " << ratio.first << "Total: " << ratio.second << '\n';
 	}
 
 };
