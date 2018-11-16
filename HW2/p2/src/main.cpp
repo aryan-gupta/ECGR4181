ebb94324b7a49acef5374aab12832a2082efa13f
Aryan Gupta <kuantum.freak@gmail.com>
2018-11-15 21:34:51 -0500

    Update RC test and add Makefile

--- HW2/p2/src/main.cpp
+++ HW2/p2/src/main.cpp
@@ -5,21 +5,27 @@
 #include <iostream>
 #include <cstring>
 
-constexpr size_t gMag = 10'000UL; // size of the 2D array (gMag x gMag)
+/* constexpr */ size_t gMag = 10'000UL; // size of the 2D array (gMag x gMag)
 
 using Vector2D = std::vector<std::vector<uint8_t>>;
 
 int main(int argc, char** argv) {
+	if (argc >= 2) {
+		gMag = std::atol(argv[1]);
+	}
+
+	std::cout << "Running Row/Column Major test with " << gMag << " iterations" << std::endl;
+
 	using hrc = std::chrono::high_resolution_clock;
 
 	Vector2D test1;
 	test1.resize(gMag);
 	std::for_each(test1.begin(), test1.end(), [](auto&& t){ t.resize(gMag); });
 
-	//// COLUMN MAJOR TEST ////
+	//// ROW MAJOR TEST ////
 	auto start = hrc::now();
-	for (int i = 0; i < gMag; ++i) {
-		for (int j = 0; j < gMag; ++j) {
+	for (unsigned i = 0; i < gMag; ++i) {
+		for (unsigned j = 0; j < gMag; ++j) {
 			test1[i][j] = 5; // sets each element to 5
 		}
 	}
@@ -28,7 +34,7 @@ int main(int argc, char** argv) {
 	test1.clear(); // this will in-turn clean all the inner vectors
 
 	std::chrono::duration<double, std::milli> elapse = end - start;
-	std::cout << "Column major test took: " << elapse.count() << "ms" << std::endl;
+	std::cout << "Row major test took: " << elapse.count() << "ms" << std::endl;
 
 
 
@@ -36,15 +42,15 @@ int main(int argc, char** argv) {
 	test2.resize(gMag);
 	std::for_each(test2.begin(), test2.end(), [](auto&& t){ t.resize(gMag); });
 
-	//// ROW MAJOR TEST ////
+	//// COLUMN MAJOR TEST ////
 	start = hrc::now();
-	for (int i = 0; i < gMag; ++i) {
-		for (int j = 0; j < gMag; ++j) {
+	for (unsigned i = 0; i < gMag; ++i) {
+		for (unsigned j = 0; j < gMag; ++j) {
 			test2[j][i] = 5;
 		}
 	}
 	end = hrc::now();
 
 	elapse = end - start;
-	std::cout << "Row major test took: " << elapse.count() << "ms" << std::endl;
+	std::cout << "Column major test took: " << elapse.count() << "ms" << std::endl;
 }
