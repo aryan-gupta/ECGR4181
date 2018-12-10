05a14a7c20e327024e97f0dfebaea4cae4891a24
Aryan Gupta <kuantum.freak@gmail.com>
2018-12-10 16:08:47 -0500

    Update Doc for branch predictor interface

--- FP/src/BranchPredictor.hpp
+++ FP/src/BranchPredictor.hpp
@@ -16,15 +16,20 @@ struct BranchPredictor {
 	// So we can do delete from a base class
 	virtual ~BranchPredictor() = default;
 
-	// This operator returns if we should take the branch or not take it. I takes in an
-	// address and uses whatever meathod to determine its prediction
-	// The b object is the target of the branch
+	/* This operator returns if we should take the branch or not take it. I takes in the
+	 * branch's address, the target address and any other ISA extensions and uses it to
+	 * determine its prediction
+	 * @param a The branch's address
+	 * @param b The target's address
+	 * @param c The ISA Extensions we could have
+	 */
 	virtual bool operator()(addr_t a, addr_t b, Extensions c) = 0;
 
-	// This operator updates its database depending on the actual.
-	// @param a The address of the branch
-	// @param b Whether the branch was taken or not
-	// @param c Our prediction from above operator. This is to prevent recalculation.
+	/* This operator updates its database depending on the actual.
+	 * @param a The address of the branch
+	 * @param b Whether the branch was taken or not
+	 * @param c Our prediction from above operator. This is to prevent recalculation.
+	 */
 	virtual void operator()(addr_t a, bool b, bool c) = 0;
 };
 
